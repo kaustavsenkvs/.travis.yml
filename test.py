baa54810184402ecec71f08df94d97e422c1c264
kaustavsenkvs <48134998+kaustavsenkvs@users.noreply.github.com>
2019-03-07 22:42:56 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from calculator import subtraction
 from calculator import multiplication
 from calculator import division
 
-def test_add()
+def test_add():
   assert add(2,6)==8
 
-def test_wrong_add()
+def test_wrong_add():
   assert add(2,6)==7
 
-def test_subtract()
+def test_subtract():
   assert subtraction(2,6)==-4
   
-def test_multiply()
+def test_multiply():
   assert multiplication(2,9)==18
   
-def test_divide()
+def test_divide():
   assert division(3,1)==1
